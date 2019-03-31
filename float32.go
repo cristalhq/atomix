6b612739ff74612fe9d08547217f90f3547245b6
Oleg Kovalov <iamolegkovalov@gmail.com>
2019-03-31 14:28:37 +0200

    float32

---
+++ float32.go
@@ -0,0 +1,47 @@
+package atomix
+
+import (
+	"math"
+	"sync/atomic"
+)
+
+// Float32 is an atomic wrapper around float32.
+type Float32 struct {
+	v uint32
+}
+
+// NewFloat32 creates a float32.
+func NewFloat32(f float32) *Float32 {
+	return &Float32{math.Float32bits(f)}
+}
+
+// Load atomically the value.
+func (f *Float32) Load() float32 {
+	return math.Float32frombits(atomic.LoadUint32(&f.v))
+}
+
+// Store atomically the passed value.
+func (f *Float32) Store(s float32) {
+	atomic.StoreUint32(&f.v, math.Float32bits(s))
+}
+
+// Add atomically and return the new value.
+func (f *Float32) Add(s float32) float32 {
+	for {
+		old := f.Load()
+		new := old + s
+		if f.CAS(old, new) {
+			return new
+		}
+	}
+}
+
+// Sub atomically and return the new value.
+func (f *Float32) Sub(s float32) float32 {
+	return f.Add(-s)
+}
+
+// CAS is an atomic Compare-and-swap.
+func (f *Float32) CAS(old, new float32) bool {
+	return atomic.CompareAndSwapUint32(&f.v, math.Float32bits(old), math.Float32bits(new))
+}
---
+++ float32_test.go
@@ -0,0 +1,19 @@
+package atomix
+
+import "testing"
+
+func TestFloat32(t *testing.T) {
+	a := NewFloat32(10.5)
+
+	Equal(t, float32(10.5), a.Load(), "Load wrong value")
+
+	Equal(t, float32(10.8), a.Add(0.3), "Add wrong value")
+	Equal(t, float32(10.3), a.Sub(0.5), "Sub wrong value")
+
+	OK(t, a.CAS(10.3, 0.5), "CAS should swap")
+	Equal(t, float32(0.5), a.Load(), "CAS wrong value")
+	NotOK(t, a.CAS(0.0, 1.5), "CAS shouldn't swap")
+
+	a.Store(42.0)
+	Equal(t, float32(42.0), a.Load(), "Store wrong value")
+}
