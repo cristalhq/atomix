509d16dd63cbf155aaa4238e3c79e9b075e8f1de
Vinod Reddy Kotha <vinodreddy9876@gmail.com>
2019-10-02 10:52:34 +0530

    implemented atomic complex64 (#5)

--- complex64.go
+++ complex64.go
@@ -7,48 +7,49 @@ import (
 
 // Complex64 is an atomic wrapper around float32.
 type Complex64 struct {
-	r uint32
-	i uint32
+	ri uint64
 }
 
-// NewComplex65 creates a float32.
-func NewComplex64(f complex64) *Complex64 {
-	return &Complex64{
-		math.Float32bits(real(f)),
-		math.Float32bits(imag(f)),
-	}
+func uint64ToComplex64(u uint64) complex64 {
+	return complex(math.Float32frombits(uint32(u>>32)), math.Float32frombits(uint32((u<<32)>>32)))
+}
+
+func complex64ToUint64(c complex64) uint64 {
+	return uint64(math.Float32bits(real(c)))<<32 | uint64(math.Float32bits(imag(c)))
+}
+
+// NewComplex64 creates a float32.
+func NewComplex64(c complex64) *Complex64 {
+	return &Complex64{complex64ToUint64(c)}
 }
 
 // Load atomically the value.
-func (f *Complex64) Load() complex64 {
-	return complex(math.Float32frombits(atomic.LoadUint32(&f.r)), math.Float32frombits(atomic.LoadUint32(&f.i)))
+func (c *Complex64) Load() complex64 {
+	return uint64ToComplex64(atomic.LoadUint64(&c.ri))
 }
 
 // Store atomically the passed value.
-func (f *Complex64) Store(s complex64) {
-	atomic.StoreUint32(&f.r, math.Float32bits(real(s)))
-	atomic.StoreUint32(&f.i, math.Float32bits(imag(s)))
+func (c *Complex64) Store(s complex64) {
+	atomic.StoreUint64(&c.ri, complex64ToUint64(s))
 }
 
 // Add atomically and return the new value.
-func (f *Complex64) Add(s complex64) complex64 {
+func (c *Complex64) Add(s complex64) complex64 {
 	for {
-		old := f.Load()
-		new := old + s
-		if f.CAS(old, new) {
-			return new
+		oc := c.Load()
+		nc := oc + s
+		if c.CAS(oc, nc) {
+			return nc
 		}
 	}
 }
 
 // Sub atomically and return the new value.
-func (f *Complex64) Sub(s complex64) complex64 {
-	return f.Add(-s)
+func (c *Complex64) Sub(s complex64) complex64 {
+	return c.Add(-s)
 }
 
 // CAS is an atomic Compare-and-swap.
-func (f *Complex64) CAS(old, new complex64) bool {
-	r := atomic.CompareAndSwapUint32(&f.r, math.Float32bits(real(old)), math.Float32bits(real(new)))
-	i := atomic.CompareAndSwapUint32(&f.i, math.Float32bits(imag(old)), math.Float32bits(imag(new)))
-	return r && i
+func (c *Complex64) CAS(oc, nc complex64) bool {
+	return atomic.CompareAndSwapUint64(&c.ri, complex64ToUint64(oc), complex64ToUint64(nc))
 }
