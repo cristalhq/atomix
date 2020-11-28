2c3bfde08bb3ef906b6fdae2799e9df214f5f401
Oleg Kovalov <oleg@hey.com>
2020-11-28 16:34:04 +0100

    add time.Duration

---
+++ duration.go
@@ -0,0 +1,49 @@
+package atomix
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+// Duration is an atomic wrapper around an time.Duration.
+type Duration struct {
+	noCopy
+	value int64
+}
+
+// NewDuration creates a Duration.
+func NewDuration(d time.Duration) *Duration {
+	return &Duration{
+		value: int64(d),
+	}
+}
+
+// Load atomically the value.
+func (d *Duration) Load() time.Duration {
+	return time.Duration(atomic.LoadInt64(&d.value))
+}
+
+// Store atomically the passed value.
+func (d *Duration) Store(dur time.Duration) {
+	atomic.StoreInt64(&d.value, int64(dur))
+}
+
+// Swap atomically and return the old value.
+func (d *Duration) Swap(dur time.Duration) time.Duration {
+	return time.Duration(atomic.SwapInt64(&d.value, int64(dur)))
+}
+
+// Add atomically and return the new value.
+func (d *Duration) Add(dur time.Duration) time.Duration {
+	return time.Duration(atomic.AddInt64(&d.value, int64(dur)))
+}
+
+// Sub atomically and return the new value.
+func (d *Duration) Sub(dur time.Duration) time.Duration {
+	return time.Duration(atomic.AddInt64(&d.value, -int64(dur)))
+}
+
+// CAS is an atomic Compare-and-swap.
+func (d *Duration) CAS(old, new time.Duration) bool {
+	return atomic.CompareAndSwapInt64(&d.value, int64(old), int64(new))
+}
---
+++ duration_test.go
@@ -0,0 +1,25 @@
+package atomix
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDuration(t *testing.T) {
+	a := NewDuration(10 * time.Second)
+
+	Equal(t, 10*time.Second, a.Load(), "Load wrong value")
+
+	Equal(t, 15*time.Second, a.Add(5*time.Second), "Add wrong value")
+	Equal(t, 12*time.Second, a.Sub(3*time.Second), "Sub wrong value")
+
+	OK(t, a.CAS(12*time.Second, 0), "CAS should swap")
+	Equal(t, 0*time.Second, a.Load(), "CAS wrong value")
+	NotOK(t, a.CAS(13*time.Second, 0), "CAS should not swap")
+
+	Equal(t, 0*time.Second, a.Swap(1*time.Second), "Swap wrong value")
+	Equal(t, 1*time.Second, a.Load(), "Swap wrong value")
+
+	a.Store(15 * time.Second)
+	Equal(t, 15*time.Second, a.Load(), "Store wrong value")
+}
