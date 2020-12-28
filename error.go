d98da97aadd1461bc2b1203499bf0037712688da
Oleg Kovalov <oleg@hey.com>
2020-12-28 19:31:54 +0100

    add error

---
+++ error.go
@@ -0,0 +1,43 @@
+package atomix
+
+import "sync/atomic"
+
+// Error is an atomic wrapper around error.
+type Error struct {
+	value atomic.Value
+}
+
+var _ error = &Error{}
+
+// NewError creates an Error.
+func NewError(err error) *Error {
+	e := &Error{}
+	e.Store(err)
+	return e
+}
+
+func (e *Error) Error() string {
+	if err := e.Load(); err != nil {
+		return err.Error()
+	}
+	return "<nil>"
+}
+
+// HasError reports whether non-nil error is stored.
+func (e *Error) HasError() bool {
+	return e.Load() != nil
+}
+
+// Load atomically the value.
+func (e *Error) Load() error {
+	v := e.value.Load()
+	if v == nil {
+		return nil
+	}
+	return v.(error)
+}
+
+// Store atomically the given value.
+func (e *Error) Store(err error) {
+	e.value.Store(err)
+}
