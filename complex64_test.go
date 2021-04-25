448e8aad83ff94e71d52612a7a9e10d11e7602a7
Oleg Kovalov <oleg@hey.com>
2021-04-25 18:39:55 +0200

    refactor Complex64 test

--- complex64_test.go
+++ complex64_test.go
@@ -3,14 +3,16 @@ package atomix
 import "testing"
 
 func TestComplex64(t *testing.T) {
-	a := NewComplex64(complex(float32(10.5), float32(10.5)))
+	a := NewComplex64(complex(float32(10.5), float32(12.3)))
 
-	Equal(t, complex(float32(10.5), float32(10.5)), a.Load(), "Load wrong value")
+	Equal(t, "(10.5+12.3i)", a.String(), "Wrong String")
 
-	Equal(t, complex(float32(10.8), float32(10.8)), a.Add(complex(float32(0.3), float32(0.3))), "Add wrong value")
-	Equal(t, complex(float32(10.3), float32(10.3)), a.Sub(complex(float32(0.5), float32(0.5))), "Sub wrong value")
+	Equal(t, complex(float32(10.5), 12.3), a.Load(), "Load wrong value")
 
-	OK(t, a.CAS(complex(float32(10.3), float32(10.3)), complex(float32(0.5), float32(0.5))), "CAS should swap")
+	Equal(t, complex(float32(10.8), float32(12.6)), a.Add(complex(float32(0.3), float32(0.3))), "Add wrong value")
+	Equal(t, complex(float32(10.3), float32(12.1)), a.Sub(complex(float32(0.5), float32(0.5))), "Sub wrong value")
+
+	OK(t, a.CAS(complex(float32(10.3), float32(12.1)), complex(float32(0.5), float32(0.5))), "CAS should swap")
 	Equal(t, complex(float32(0.5), float32(0.5)), a.Load(), "CAS wrong value")
 	NotOK(t, a.CAS(complex(float32(0.0), float32(0.0)), complex(float32(1.5), float32(1.5))), "CAS shouldn't swap")
 
