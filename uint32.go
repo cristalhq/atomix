c6830d7694b96ee551f926f8f655854bd6bc4eb8
Oleg Kovalov <oleg@hey.com>
2022-04-17 13:10:48 +0200

    Uint renames

--- uint32.go
+++ uint32.go
@@ -16,71 +16,71 @@ func NewUint32(i uint32) *Uint32 {
 	return &Uint32{value: i}
 }
 
-func (i *Uint32) String() string {
-	return strconv.FormatUint(uint64(i.Load()), 10)
+func (u *Uint32) String() string {
+	return strconv.FormatUint(uint64(u.Load()), 10)
 }
 
 // Load atomically the value.
-func (i *Uint32) Load() uint32 {
-	return atomic.LoadUint32(&i.value)
+func (u *Uint32) Load() uint32 {
+	return atomic.LoadUint32(&u.value)
 }
 
 // Store atomically the given value.
-func (i *Uint32) Store(n uint32) {
-	atomic.StoreUint32(&i.value, n)
+func (u *Uint32) Store(n uint32) {
+	atomic.StoreUint32(&u.value, n)
 }
 
 // Swap atomically and return the old value.
-func (i *Uint32) Swap(n uint32) uint32 {
-	return atomic.SwapUint32(&i.value, n)
+func (u *Uint32) Swap(n uint32) uint32 {
+	return atomic.SwapUint32(&u.value, n)
 }
 
 // Add atomically and return the new value.
-func (i *Uint32) Add(n uint32) uint32 {
-	return atomic.AddUint32(&i.value, n)
+func (u *Uint32) Add(n uint32) uint32 {
+	return atomic.AddUint32(&u.value, n)
 }
 
 // Sub atomically and return the new value.
-func (i *Uint32) Sub(n uint32) uint32 {
-	return atomic.AddUint32(&i.value, ^(n - 1))
+func (u *Uint32) Sub(n uint32) uint32 {
+	return atomic.AddUint32(&u.value, ^(n - 1))
 }
 
 // Inc atomically and return the new value.
-func (i *Uint32) Inc() uint32 {
-	return i.Add(1)
+func (u *Uint32) Inc() uint32 {
+	return u.Add(1)
 }
 
 // Dec atomically and return the new value.
-func (i *Uint32) Dec() uint32 {
-	return i.Sub(1)
+func (u *Uint32) Dec() uint32 {
+	return u.Sub(1)
 }
 
 // CAS is an atomic Compare-And-Swap operation.
-func (i *Uint32) CAS(old, new uint32) bool {
-	return atomic.CompareAndSwapUint32(&i.value, old, new)
+func (u *Uint32) CAS(old, new uint32) bool {
+	return atomic.CompareAndSwapUint32(&u.value, old, new)
 }
 
 // SwapGreater value atomically, returns old and swap result.
-func (i *Uint32) SwapGreater(new uint32) (old uint32, swapped bool) {
+func (u *Uint32) SwapGreater(new uint32) (old uint32, swapped bool) {
 	for {
-		old := i.Load()
+		old := u.Load()
 		if new <= old {
 			return old, false
 		}
-		if i.CAS(old, new) {
+		if u.CAS(old, new) {
 			return old, true
 		}
 	}
 }
 
 // SwapLess value atomically, returns old and swap result.
-func (i *Uint32) SwapLess(new uint32) (old uint32, swapped bool) {
+func (u *Uint32) SwapLess(new uint32) (old uint32, swapped bool) {
 	for {
-		old := i.Load()
+		old := u.Load()
 		if new >= old {
 			return old, false
 		}
-		if i.CAS(old, new) {
+		if u.CAS(old, new) {
 			return old, true
 		}
 	}
--- uint64.go
+++ uint64.go
@@ -16,71 +16,71 @@ func NewUint64(i uint64) *Uint64 {
 	return &Uint64{value: i}
 }
 
-func (i *Uint64) String() string {
-	return strconv.FormatUint(i.Load(), 10)
+func (u *Uint64) String() string {
+	return strconv.FormatUint(u.Load(), 10)
 }
 
 // Load atomically the value.
-func (i *Uint64) Load() uint64 {
-	return atomic.LoadUint64(&i.value)
+func (u *Uint64) Load() uint64 {
+	return atomic.LoadUint64(&u.value)
 }
 
 // Store atomically the given value.
-func (i *Uint64) Store(n uint64) {
-	atomic.StoreUint64(&i.value, n)
+func (u *Uint64) Store(n uint64) {
+	atomic.StoreUint64(&u.value, n)
 }
 
 // Swap atomically and return the old value.
-func (i *Uint64) Swap(n uint64) uint64 {
-	return atomic.SwapUint64(&i.value, n)
+func (u *Uint64) Swap(n uint64) uint64 {
+	return atomic.SwapUint64(&u.value, n)
 }
 
 // Add atomically and return the new value.
-func (i *Uint64) Add(n uint64) uint64 {
-	return atomic.AddUint64(&i.value, n)
+func (u *Uint64) Add(n uint64) uint64 {
+	return atomic.AddUint64(&u.value, n)
 }
 
 // Sub atomically and return the new value.
-func (i *Uint64) Sub(n uint64) uint64 {
-	return atomic.AddUint64(&i.value, ^(n - 1))
+func (u *Uint64) Sub(n uint64) uint64 {
+	return atomic.AddUint64(&u.value, ^(n - 1))
 }
 
 // Inc atomically and return the new value.
-func (i *Uint64) Inc() uint64 {
-	return i.Add(1)
+func (u *Uint64) Inc() uint64 {
+	return u.Add(1)
 }
 
 // Dec atomically and return the new value.
-func (i *Uint64) Dec() uint64 {
-	return i.Sub(1)
+func (u *Uint64) Dec() uint64 {
+	return u.Sub(1)
 }
 
 // CAS is an atomic Compare-And-Swap operation.
-func (i *Uint64) CAS(old, new uint64) bool {
-	return atomic.CompareAndSwapUint64(&i.value, old, new)
+func (u *Uint64) CAS(old, new uint64) bool {
+	return atomic.CompareAndSwapUint64(&u.value, old, new)
 }
 
 // SwapGreater value atomically, returns old and swap result.
-func (i *Uint64) SwapGreater(new uint64) (old uint64, swapped bool) {
+func (u *Uint64) SwapGreater(new uint64) (old uint64, swapped bool) {
 	for {
-		old := i.Load()
+		old := u.Load()
 		if new <= old {
 			return old, false
 		}
-		if i.CAS(old, new) {
+		if u.CAS(old, new) {
 			return old, true
 		}
 	}
 }
 
 // SwapLess value atomically, returns old and swap result.
-func (i *Uint64) SwapLess(new uint64) (old uint64, swapped bool) {
+func (u *Uint64) SwapLess(new uint64) (old uint64, swapped bool) {
 	for {
-		old := i.Load()
+		old := u.Load()
 		if new >= old {
 			return old, false
 		}
-		if i.CAS(old, new) {
+		if u.CAS(old, new) {
 			return old, true
 		}
 	}
